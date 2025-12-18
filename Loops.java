8b970cb066cb325181e74068868d026928672397
Sofia <samunoz.flower@gmail.com>
2025-12-18 01:22:40 +0000

    Completes Loops.java Challenges

--- Loops.java
+++ Loops.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Loops {    
     public static void main(String[] args) {/*
         // break
@@ -29,11 +32,30 @@ public class Loops {
 // Challenge 1
 for (int i = 0; i<= 100; i ++){
     if (i % 3 == 0){
-        System.out.print("foo");
-    }
+        System.out.print("foo");}
     if (i % 5 == 0){
-        System.out.print("bar");
-    }
+        System.out.print("bar");}
+    System.out.println();
+} System.out.println();
+
+// Challenge 2
+System.out.println("Multiplication Table:\n*");
+for (int a = 1; a <= 12; a += 1){
+    for (int b = 1; b <= 12; b += 1) { // Print out a multiplication table from 1-12
+        System.out.print(a*b + "\t");}
     System.out.println();
 }
-}}
+// Challenge 3
+Scanner input = new Scanner(System.in);
+System.out.println("\nWhat number are you guessing?"); // Design a guessing game between 3 numbers
+int guess = Integer.parseInt(input.nextLine());
+Random random = new Random();
+int target = random.nextInt(1, 4);
+while (guess != target){
+    System.out.println("That is the wrong answer. The number changed! Guess again."); // If you guess incorrectly, then game keeps playing.
+    target = random.nextInt(1, 4); // The number changes every time you guess wrong.
+    System.out.println("\nWhat number are you guessing?");
+    guess = Integer.parseInt(input.nextLine());}
+System.out.println("That is the correct answer! You win!"); // If you guess the number correctly then you win.
+
+input.close();}}
