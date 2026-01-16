d75be77c20dace0cb36e9bacadef0dc39670c977
Sofia <samunoz.flower@gmail.com>
2026-01-16 03:29:09 +0000

    Submits Java Arrays Practice

---
+++ Arrays/TwoDTester.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TwoDTester {
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+
+        int[][] nums = new int[3][2];
+
+        for (int i = 0; i < nums.length; i++){
+            for (int j = 0; j < nums[i].length; j++){
+                System.out.println("Enter a value for row: " + i + ", and column: " + j);
+                nums[i][j] = input.nextInt();}}
+        input.close();
+
+        // Print out the 2D array as a grid
+        for (int i = 0; i < nums.length; i++){
+            System.out.print("\n\t");
+            for (int j = 0; j < nums[i].length; j++){
+                System.out.print(nums[i][j] + "\t");}}
+        System.out.println("\n");
+    }}
