47b3b8faf8b62ba36f83572a8664d3aedf9c40d7
Sofia <samunoz.flower@gmail.com>
2026-05-08 12:35:46 +0000

    Recursion

--- FRQRunner.java
+++ FRQRunner.java
@@ -1,18 +1,23 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 public class FRQRunner {
   public static void main(String args[]){
-	import Java.util.Random
-public avoid simulateOneDay(int numBirds){
-	int bear = random.nextInt(0, 20);
-	if (bear == 0){
-		System.out.println("Bear attack!");
+	int[] list = {3, 8, 2, 5, 1};
+	int n = list.length;
+// outer loop
+for (int i = 0; i < n; i++){
+	int minIndex = i;
+	// inner loop
+	for (int j = i + 1; j < n; j++){
+		// conditional logic to compare values
+		if (list[j] < list[minIndex]){
+			minIndex = j;
+		}
 	}
-	Random random = new Random();
-	int food = random.nextInt(10, 51);
-	int eaten = food * numBirds;
-	currentFood -= eaten;
-	if (currentFood <= 0){
-		System.out.println("Feeder is empty.");
-		currentFood = 0;
-	}
+	// swap numbers
+	int temp = list[i];
+	list [i] = list[minIndex];
+	list[minIndex] = temp;
+	System.out.println(Arrays.toString(list));
+}
+System.out.println(Arrays.toString(list));
 }}
